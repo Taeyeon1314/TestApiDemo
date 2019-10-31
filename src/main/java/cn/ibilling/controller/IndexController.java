0d4228ba96a119d85e1c6734ca3b096084e43e86
韩小杰 <hanxiaojie@i-billing.cn>
2019-10-31 19:22:08 +0800

    修改接口内容

--- src/main/java/cn/ibilling/controller/IndexController.java
+++ src/main/java/cn/ibilling/controller/IndexController.java
@@ -9,6 +9,7 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -18,9 +19,9 @@ public class IndexController {
 
     @ApiOperation(value = "根据用户名查询用户信息", notes = "查询数据库中某个的用户信息")
     @ApiImplicitParam(name = "username", value = "用户名", paramType = "path", required = true, dataType = "String")
-    @GetMapping(value = "/user/{username}")
+    @GetMapping(value = "/getUserByName")
     @ResponseBody
-    public Result getUserByName(@PathVariable String username) {
+    public Result getUserByName(@RequestParam("username") String username) {
         if (username.equals("admin")) {
             return new Result("0", "查询成功", new User(username, "1234567"));
         } else {
@@ -30,9 +31,9 @@ public class IndexController {
 
     @ApiOperation(value = "根据用户ID查询用户信息", notes = "查询数据库中某个的用户信息")
     @ApiImplicitParam(name = "id", value = "用户id", paramType = "path", required = true, dataType = "Integer")
-    @GetMapping(value = "/user/{id}")
+    @GetMapping(value = "/user/findUserById")
     @ResponseBody
-    public Result getUserById(@PathVariable Integer id) {
+    public Result getUserById(@RequestParam("id") Integer id) {
         return new Result("0", "查询成功", new User("admin", "1234567"));
     }
 
@@ -43,7 +44,16 @@ public class IndexController {
     })
     @PostMapping("/login")
     @ResponseBody
-    public Result testSwagger(String username, String password) {
-        return new Result("0", "请求成功", new User(username, password));
+    public Result testSwagger(@RequestBody User user) {
+        if (!StringUtils.isEmpty(user)) {
+            if ("admin".equals(user.getUsername()) && "1234567".equals(user.getPassword())) {
+                return new Result("0", "请求成功", new User(user.getUsername(), user.getPassword()));
+            } else {
+                return new Result("1", "用户名和密码不匹配", null);
+            }
+        } else {
+            return new Result("1", "请求失败", null);
+
+        }
     }
 }
